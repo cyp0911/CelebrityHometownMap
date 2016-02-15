729a9a68b692fc0edd5724ceb24d1777ac78e71f
cyp0911 <cyp0911@gmail.com>
2016-02-14 21:50:09 -0400

    Initial Commit

---
+++ 名人故里地图/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  名人故里地图
+//
+//  Created by 陈音澎 on 2/14/16.
+//  Copyright © 2016 Clark Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
